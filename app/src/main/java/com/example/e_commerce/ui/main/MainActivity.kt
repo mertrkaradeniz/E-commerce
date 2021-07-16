8464ac26c90a59c83ec4426a42a0653d0fc2b806
Mert <mertrizakaradeniz@gmail.com>
2021-07-16 17:45:13 +0300

    Bottom navigation view setup

--- app/src/main/java/com/example/e_commerce/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/main/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.e_commerce.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.e_commerce.R
+import com.example.e_commerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+//    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBottomNavigation()
     }
+
+    private fun setupBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+//        appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.productsFragment,
+//                R.id.dashboardFragment,
+//                R.id.labelFragment,
+//                R.id.profileFragment
+//            )
+//        )
+        binding.bottomNavigationView.setupWithNavController(navController)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+
+
 }
